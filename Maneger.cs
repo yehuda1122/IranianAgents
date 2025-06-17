e7314912d4a0598a8f41c62456d04c92b5fbd65f
yehuda <yehudaperetz96@gmail.com>
2025-06-17 09:46:34 +0300

    Inserting sensors into random vulnerabilities

--- Maneger.cs
+++ Maneger.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace IranianAgents
 {
-    internal class Maneger
+    public class Maneger
     {
+        public static class AllSensor
+        {
+            public static List<ISensor> SensorType = new List<ISensor>
+    {
+        new TrafficSensor(),
+        new ThermalSensor(),
+        new CellularSensor()
+    };
+        }
+
+        SimpleAgent agent = new SimpleAgent("Ali");
+        Random random = new Random();
+        public void AddSensorToAgent()
+        {
+            for (int i = 0; i < agent.SecretWeaknesses.Length; i++)
+            {
+                int index = random.Next(AllSensor.SensorType.Count); // בוחר אינדקס רנדומלי
+                var sensorType = AllSensor.SensorType[index]; // לוקח סנסור מהרשימה
+                                                              // יוצר עותק חדש מאותו סוג (חשוב!)
+                if (sensorType is TrafficSensor)
+                    agent.SecretWeaknesses[i] = new TrafficSensor();
+                else if (sensorType is ThremalSensor)
+                    agent.SecretWeaknesses[i] = new ThremalSensor();
+                else if (sensorType is CellularSensor)
+                    agent.SecretWeaknesses[i] = new CellularSensor();
+            }
+        }
     }
 }
